8789e7660133a75d3f6f6b4637cb070532eabea8
brouardt <34036880+brouardt@users.noreply.github.com>
2018-04-28 11:04:10 +0200

    deleted = 0
    ajout du filtre deleted = 0

--- includes/paralog_person.php
+++ includes/paralog_person.php
@@ -117,7 +117,7 @@ class Paralog_Person extends Paralog_Table
         );
         $this->items = $wpdb->get_results($query, ARRAY_A);
 
-        $total_items = $wpdb->get_var("SELECT COUNT(*) FROM $table");
+        $total_items = $wpdb->get_var("SELECT COUNT(*) FROM $table WHERE deleted = 0");
 
         $this->set_pagination_args(array(
             'total_items' => $total_items,
